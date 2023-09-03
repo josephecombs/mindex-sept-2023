599785984397c3295519588f86ba582e8a347872
josephecombs <joseph.e.combs@gmail.com>
2023-09-03 15:45:09 -0400

    add RS model

---
+++ CodeChallenge/Models/ReportingStructure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeChallenge.Models
+{
+    public class ReportingStructure
+    {
+        public Employee Employee { get; set; }
+        public int NumberOfReports { get; set; }
+
+        public ReportingStructure(Employee employee, int numberOfReports)
+        {
+            this.Employee = employee;
+            this.NumberOfReports = numberOfReports;
+        }
+    }
+}
